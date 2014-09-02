cab63f1321e9e613e900b0bbe6ce19261eda3b0a
tshap88 <tshap88@gmail.com>
2014-09-02 14:53:24 -0700

    added thanks.html

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,13 +14,14 @@ urlpatterns = patterns('',
     url(r'^about_us/$', views.about, name = 'about'),
     url(r'^blog/$', views.blog, name = 'blog'),
     url(r'^contact/$', views.contact, name = 'contact'),
+    url(r'^contact/thanks/$', views.thanks, name='thanks'),
     url(r'^news/', include('news.urls', namespace = 'news')),
 
 )
-handler404 = 'news.views.custom_404'
-handler500 = 'news.views.custom_500'
-handler400 = 'news.views.custom_400'
-handler403 = 'news.views.custom_403'
+#handler404 = 'news.views.custom_404'
+#handler500 = 'news.views.custom_500'
+#handler400 = 'news.views.custom_400'
+#handler403 = 'news.views.custom_403'
 
 from django.conf import settings
 if settings.DEBUG:
